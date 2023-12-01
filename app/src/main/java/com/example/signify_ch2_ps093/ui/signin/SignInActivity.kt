73f98e6161a663e880b02a7f3f7eda791d0d3bd9
Muhammad_Asandy Putra <asandyputra77@gmail.com>
2023-12-02 05:57:24 +0800

    Commit On Login and SignUp UI

--- app/src/main/java/com/example/signify_ch2_ps093/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/example/signify_ch2_ps093/ui/signin/SignInActivity.kt
@@ -1,8 +1,10 @@
 package com.example.signify_ch2_ps093.ui.signin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.signify_ch2_ps093.databinding.ActivitySignInBinding
+import com.example.signify_ch2_ps093.ui.login.LoginActivity
 
 class SignInActivity : AppCompatActivity() {
 
@@ -12,5 +14,12 @@ class SignInActivity : AppCompatActivity() {
 
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.tvDirectToLogin.setOnClickListener{
+            val intent = Intent(this@SignInActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+//            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+        }
     }
 }
