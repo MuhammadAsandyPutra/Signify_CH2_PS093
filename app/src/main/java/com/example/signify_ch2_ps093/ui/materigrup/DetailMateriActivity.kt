493c1cacb72c332c2d593f6e79c773bf251a00aa
Niwanaf <niwanaf@gmail.com>
2023-12-04 17:58:59 +0700

    add ui for detail materi and quiz

---
+++ app/src/main/java/com/example/signify_ch2_ps093/ui/materigrup/DetailMateriActivity.kt
@@ -0,0 +1,12 @@
+package com.example.signify_ch2_ps093.ui.materigrup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.signify_ch2_ps093.R
+
+class DetailMateriActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_materi)
+    }
+}
---
+++ app/src/main/java/com/example/signify_ch2_ps093/ui/quiz/QuizActivity.kt
@@ -0,0 +1,12 @@
+package com.example.signify_ch2_ps093.ui.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.signify_ch2_ps093.R
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
