58aaed6f30a79a9ad347fe66edd44db40e2703b2
Muhammad_Asandy Putra <asandyputra77@gmail.com>
2023-12-05 11:02:48 +0800

    QuizDetail update

---
+++ app/src/main/java/com/example/signify_ch2_ps093/ui/quiz/QuizDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.signify_ch2_ps093.ui.quiz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.signify_ch2_ps093.R
+
+class QuizDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_detail)
+    }
+}
